4f3a14bda2d999d05fac9048025e9f77306fb3d2
Shivang Rawat <sr6364@nyu.edu>
2024-12-01 20:16:10 -0500

    run

--- slurm_jobs/script_generator.py
+++ slurm_jobs/script_generator.py
@@ -86,8 +86,8 @@ if __name__ == "__main__":
     }
 
     # Define model parameters
-    HIDDEN_SIZE = 128
-    PERMUTED = True
+    HIDDEN_SIZE = 64
+    PERMUTED = False
     CHECKPOINT = False
     dt_tau_max_y = 0.05
     dt_tau_max_a = 0.01
